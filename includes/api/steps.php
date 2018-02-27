f9d01423e482d6e62a1a63d7867116212cf8ba67
Faggioni <miguelfaggioni@gmail.com>
2018-02-27 13:33:24 -0400

    Api Calls

--- includes/api/steps.php
+++ includes/api/steps.php
@@ -1,11 +1,18 @@
 <?php
 
+/*
+ * Retrieved Information Regarding the Step 1 of the Reservation Flow
+ * @return
+ */
 function hq_rental_wpv2_api_get_locations_step_1()
 {
     $response = wp_remote_get(HQ_RENTAL_WPV2_FLEETS_LOCATIONS_STEP_1_URL, hq_rental_wpv2_get_header());
     return hq_rental_wpv2_response_handler($response);
 }
 
+/*
+ * Retrieves
+ */
 function hq_rental_wpv2_get_availability_step_2($post_data)
 {
     //$response = wp_remote_post(HQ_RENTAL_WPV2_CHECK_AVAILABILITY_STEP_2_URL, hq_rental_wpv2_get_http_arguments($post_data));
