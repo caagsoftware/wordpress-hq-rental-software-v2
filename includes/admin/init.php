001ef32a4166cb76b69df26415593d87fdcaa3ff
Faggioni <miguelfaggioni@gmail.com>
2018-05-01 11:09:44 -0400

    Fix Define Issue

--- includes/admin/init.php
+++ includes/admin/init.php
@@ -1,7 +1,7 @@
 <?php
 
-define(HQ_RENTAL_WPV2_SETTING_TITLE, 'HQWP Rental Setup');
-define(HQ_RENTAL_WPV2_SETTING_MENU, 'HQWP Rental Setup');
-define(HQ_RENTAL_WPV2_SETTING_SLUG, 'hq-rental-wpv2');
+define('HQ_RENTAL_WPV2_SETTING_TITLE', 'HQWP Rental Setup');
+define('HQ_RENTAL_WPV2_SETTING_MENU', 'HQWP Rental Setup');
+define('HQ_RENTAL_WPV2_SETTING_SLUG', 'hq-rental-wpv2');
 require_once('settings.php');
 require_once('admin-utils.php');
